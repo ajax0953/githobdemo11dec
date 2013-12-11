898fe98a1703c1c0e33deb30d45c4d7e842a06ee
Jason.Chien <ajax.chien@gmail.com>
2013-12-11 20:44:18 +0800

    Init Project Data

---
+++ AndroidTestee/gen/tw/java2/androidtestee/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package tw.java2.androidtestee;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ AndroidTestee/src/tw/java2/androidtestee/MainActivity.java
@@ -0,0 +1,33 @@
+package tw.java2.androidtestee;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	private final static String TAG = "MYTEST";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public void doClick(View view) {
+		Log.v(TAG, "Button Click !!!");
+		TextView textView1 = (TextView) findViewById(R.id.textView1);
+		EditText editText1 = (EditText) findViewById(R.id.editText1);
+		textView1.setText(editText1.getText().toString());
+	}
+}
